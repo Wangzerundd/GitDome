f1dac6b84c95665dde31a1e44d3850badf12d9a8
Wangzerundd <aaadada.rm@qq.com>
2019-06-09 20:34:56 +0800

    第三次改动 2019年6月9日20:34:50

--- app/src/main/java/com/bawei/gitdome/MainActivity.java
+++ app/src/main/java/com/bawei/gitdome/MainActivity.java
@@ -2,6 +2,7 @@ package com.bawei.gitdome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //注释：演示
         TextView tv = new TextView(MainActivity.this, null);
+        //创建一个按钮
+        Button button = new Button(MainActivity.this, null);
     }
 }
